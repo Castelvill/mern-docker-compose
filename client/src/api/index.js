1cac894aa8bc50d57027ccb05b1429beb3104a64
Castelvill <789mati@wp.pl~>
2023-05-25 21:25:11 +0100

    init

---
+++ client/src/api/index.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://192.168.56.103:5000/api', //in place of the address insert your machine's ip address
+})
+
+export const insertMovie = payload => api.post(`/movie`, payload)
+export const getAllMovies = () => api.get(`/movies`)
+export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
+export const deleteMovieById = id => api.delete(`/movie/${id}`)
+export const getMovieById = id => api.get(`/movie/${id}`)
+
+const apis = {
+    insertMovie,
+    getAllMovies,
+    updateMovieById,
+    deleteMovieById,
+    getMovieById,
+}
+
+export default apis
